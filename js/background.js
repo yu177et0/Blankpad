9f09ec11e588bedc2e542e6f7a992fc4b5d507d4
Yumeto <terada.yumeto@gmail.com>
2023-04-21 05:21:48 -0500

    To conform Google program policy. Update manifest version to 3.

--- js/background.js
+++ js/background.js
@@ -1,3 +1,3 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({url: 'html/blank.html' });
+chrome.action.onClicked.addListener(async (tab) => {
+  chrome.tabs.create({url: 'html/blank.html' });
 });
